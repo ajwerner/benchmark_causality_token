af41c90d9d4e2ddecb04aba575d4c200d37f3581
Andrew Werner <ajwerner@cockroachlabs.com>
2022-05-04 17:59:44 +0000

    initial commit

---
+++ contention.go
@@ -0,0 +1,238 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/cockroachdb/apd"
+	"github.com/cockroachdb/cockroach-go/v2/crdb/crdbpgx"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/log/logrusadapter"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+
+	var conn string
+	var par int
+	var n int
+	var test string
+	flag.StringVar(&conn, "pgurl", "", "Connection port to CRDB cluster.")
+	flag.IntVar(&par, "par", 100, "asdf")
+	flag.IntVar(&n, "n", 10000, "asdf")
+	flag.StringVar(&test, "test", "withTxn", "one of withTxn, withSelect, raw")
+	flag.Parse()
+	ctx := context.Background()
+	cfg, err := pgxpool.ParseConfig(conn)
+	if err != nil {
+		logrus.WithError(err).Fatal("failed to parse test connection")
+	}
+	cfg.ConnConfig.LogLevel = pgx.LogLevelError
+	cfg.ConnConfig.Logger = logrusadapter.NewLogger(logrus.New())
+	cfg.MaxConns = 128
+	client, err := pgxpool.ConnectConfig(ctx, cfg)
+	if err != nil {
+		logrus.WithError(err).Fatal("failed to connect to crdb")
+	}
+
+	for _, stmt := range []string{
+		`CREATE TABLE IF NOT EXISTS kv
+			(
+				key VARCHAR(512) NOT NULL PRIMARY KEY,
+				value BLOB NOT NULL
+			);`,
+		`CREATE TABLE IF NOT EXISTS test
+			(
+				id UUID DEFAULT gen_random_uuid() NOT NULL PRIMARY KEY
+			);`,
+		`SET CLUSTER SETTING kv.rangefeed.enabled = true;`,
+		`SET CLUSTER SETTING changefeed.experimental_poll_interval = '0.2s';`,
+	} {
+		if _, err := client.Exec(ctx, stmt); err != nil {
+			logrus.WithError(err).Fatal("error initializing the database")
+		}
+	}
+
+	var op func(context.Context) error
+	switch test {
+	case "raw":
+		op = raw(client)
+	case "withSelect":
+		op = withRead(client)
+	case "withTxn":
+		op = withTxn(client)
+	case "withToken":
+		op = withToken(client)
+	}
+
+	if err := do(ctx, n, par, op, test); err != nil {
+		logrus.WithError(err).Fatal("failed to interact with CRDB")
+	}
+}
+
+// raw inserts, no contention since we have no SELECT
+func raw(client *pgxpool.Pool) func(ctx context.Context) error {
+	return func(ctx context.Context) error {
+		_, err := client.Exec(ctx, `INSERT INTO test (id) VALUES (DEFAULT);`)
+		return err
+	}
+}
+
+// withRead has a SELECT afterwards, no contention
+func withRead(client *pgxpool.Pool) func(ctx context.Context) error {
+	return func(ctx context.Context) error {
+		var id uuid.UUID
+		err := client.QueryRow(ctx, `INSERT INTO test (id) VALUES (DEFAULT) RETURNING id;`).Scan(&id)
+		if err != nil {
+			return err
+		}
+		var hybridLogicalTimestamp apd.Decimal
+		return client.QueryRow(ctx, `SELECT crdb_internal_mvcc_timestamp FROM test WHERE id=$1;`, id).Scan(&hybridLogicalTimestamp)
+	}
+}
+
+// withTxn has a transaction on the insert, all of a sudden we see contention on the SELECT
+func withTxn(client *pgxpool.Pool) func(ctx context.Context) error {
+	return func(ctx context.Context) error {
+		var id uuid.UUID
+		if err := crdbpgx.ExecuteTx(ctx, client, pgx.TxOptions{}, func(tx pgx.Tx) error {
+			if _, err := tx.Exec(ctx, `INSERT INTO kv (key, value) VALUES ($1, $2);`, uuid.New().String(), strings.Repeat("x", 10000)); err != nil {
+				return err
+			}
+
+			return tx.QueryRow(ctx, `INSERT INTO test (id) VALUES (DEFAULT) RETURNING id;`).Scan(&id)
+		}); err != nil {
+			return err
+		}
+
+		var hybridLogicalTimestamp apd.Decimal
+		return client.QueryRow(ctx, `SELECT crdb_internal_mvcc_timestamp FROM test WHERE id=$1;`, id).Scan(&hybridLogicalTimestamp)
+	}
+}
+
+// withTxn has a transaction on the insert, all of a sudden we see contention on the SELECT
+func withToken(client *pgxpool.Pool) func(ctx context.Context) error {
+	return func(ctx context.Context) error {
+		var hybridLogicalTimestamp apd.Decimal
+		if err := crdbpgx.ExecuteTx(ctx, client, pgx.TxOptions{}, func(tx pgx.Tx) error {
+			if _, err := tx.Exec(ctx, `INSERT INTO kv (key, value) VALUES ($1, $2);`, uuid.New().String(), strings.Repeat("x", 10000)); err != nil {
+				return err
+			}
+
+			return tx.QueryRow(ctx, `SELECT crdb_internal.commit_with_causality_token()`).Scan(&hybridLogicalTimestamp)
+		}); err != nil {
+			return err
+		}
+		return nil
+	}
+}
+
+func do(
+	ctx context.Context,
+	iterations, parallelism int,
+	operation func(context.Context) error,
+	msg string,
+) error {
+	var durations []time.Duration
+	sink := make(chan time.Duration, parallelism)
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case duration, ok := <-sink:
+				if !ok {
+					return
+				}
+				durations = append(durations, duration)
+
+				if len(durations)%(iterations/10) == 0 {
+					summarize(durations, "processing metrics")
+				}
+			}
+		}
+	}()
+	if err := workers(ctx, iterations, parallelism, func(ctx context.Context) error {
+		before := time.Now()
+		err := operation(ctx)
+		duration := time.Since(before)
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case sink <- duration:
+		}
+		return err
+	}); err != nil {
+		return err
+	}
+	close(sink)
+	wg.Wait()
+	summarize(durations, msg)
+	return nil
+}
+
+func workers(
+	ctx context.Context, iterations, parallelism int, operation func(context.Context) error,
+) error {
+	wg := &sync.WaitGroup{}
+	operations := make(chan struct{})
+	for i := 0; i < parallelism; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case _, ok := <-operations:
+					if !ok {
+						return
+					}
+					if err := operation(ctx); err != nil {
+						logrus.WithError(err).Error("failed to interact with the API server")
+					}
+				}
+			}
+		}()
+	}
+
+	start := time.Now()
+	for i := 0; i < iterations; i++ {
+		start = display(i, iterations, start)
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case operations <- struct{}{}:
+		}
+	}
+	close(operations)
+	wg.Wait()
+	return nil
+}
+
+func display(i, max int, start time.Time) time.Time {
+	if (i+1)%(max/10) == 0 {
+		logrus.Infof("progress: %d/%d (%.0f%%); %s per ", i+1, max, 100*(float64(i+1)/float64(max)), time.Since(start)/time.Duration(max/10))
+		return time.Now()
+	}
+	return start
+}
+
+func summarize(durations []time.Duration, msg string) {
+	var all time.Duration
+	for _, duration := range durations {
+		all += duration
+	}
+	logrus.WithFields(logrus.Fields{
+		"count": len(durations),
+		"mean":  all / time.Duration(len(durations)),
+	}).Info(msg)
+}
---
+++ go.mod
@@ -0,0 +1,28 @@
+module github.com/ajwerner/benchmarking
+
+go 1.17
+
+require (
+	github.com/cockroachdb/apd v1.1.0
+	github.com/cockroachdb/cockroach-go/v2 v2.2.8
+	github.com/google/uuid v1.3.0
+	github.com/jackc/pgx/v4 v4.16.0
+	github.com/sirupsen/logrus v1.8.1
+)
+
+require (
+	github.com/gofrs/flock v0.8.1 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.12.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.11.0 // indirect
+	github.com/jackc/puddle v1.2.1 // indirect
+	github.com/lib/pq v1.10.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
